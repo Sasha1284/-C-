7ef607d9ad0817619d3bfc4c1a5c4d62a0699534
Sasha <sasha2232maximov@yandex.ru>
2024-01-03 16:23:26 +0300

    last update

--- C#/проверка.cs
+++ C#/проверка.cs
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-int main() {
+int main() 
+{
     int a;
-    scanf("%d", &a);
+    scanf ('%d',&a);
     a%2;
-    printf("%d", a%2);
+    printf('%d',a%2);
     
     return 0;
 }
